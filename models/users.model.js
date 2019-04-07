5c68ce39edb374b5fefac08aeb8ead5c8b865ebd
Waheed Afolabi <waheed.freelancer16@gmail.com>
2019-04-07 17:11:33 +0100

    setup need folders for ADC api to be used when laptop is on

---
+++ models/users.model.js
@@ -0,0 +1,14 @@
+class User {
+    constructior() {
+        this.id = null;
+        this.firstName = null;
+        this.lastName = null;
+        this.email = null;
+        this.password = null;
+        this.type = null;
+        this.phoneNumber = null;
+        this.address = null;
+        this.createdOn = null;
+        this.isAdmin = null;
+    }
+}
---
+++ utils/dummyDB.js
@@ -0,0 +1,55 @@
+import uuid from 'uuid';
+import moment from 'moment';
+
+export default {
+    users: [
+        {
+            id: uuid.v4(),
+            firstName: 'Jon',
+            lastName: 'Snow',
+            email: 'j.snow@targearyean.got',
+            password: '123456',
+            type: client,
+            phoneNumber: '08123456789',
+            address: {
+                street: '555 Bayshore Blvd',
+                city: 'Tampa',
+                state: 'Florida',
+            },
+            createdOn: moment().format('YYYY DD MM'), 
+            isAdmin: false,
+        },
+        {
+            id: uuid.v4(),
+            firstName: 'Daenarys',
+            lastName: 'Targaeryean',
+            email: 'd.targaeryean@targearyean.got',
+            password: '123456',
+            type: staff,
+            phoneNumber: '08123676589',
+            address: {
+                street: '122 Westeros Ave.',
+                city: 'Ilupeju',
+                state: 'Lagos',
+            },
+            createdOn: moment().format('YYYY DD MM'), 
+            isAdmin: false,
+        },
+        {
+            id: uuid.v4(),
+            firstName: 'Jamie',
+            lastName: 'Lannister',
+            email: 'jamielannister@lannisters.got',
+            password: '123456',
+            type: staff,
+            phoneNumber: '08053676589',
+            address: {
+                street: '122 King Slayer Ave.',
+                city: 'Idimu',
+                state: 'Lagos',
+            },
+            createdOn: moment().format('YYYY DD MM'), 
+            isAdmin: true,
+        },
+    ]
+}
